e542ed0444bbccf2fb912384f074a94ea760bfcd
root <root@keeg>
2015-03-21 06:57:33 +0800

    change files

--- sample/php_sample.h
+++ sample/php_sample.h
@@ -1,23 +1,18 @@
-#ifdef PHP_SAMPLE_H
-//防止重复包含
+#ifndef PHP_SAMPLE_H
+/* 防止重复包含 */
 #define PHP_SAMPLE_H
+/* 定义扩展的属性 */
 
-//定义扩展的属性
-#define PHP_SAMPLE_EXTNAME	"sample"
-#define PHP_SAMPLE_EXTVER		"1.0"
-
-//在PHP源码数外面构建时引入配置选项
+#define PHP_SAMPLE_EXTNAME "sample"
+#define PHP_SAMPLE_EXTVER "1.0"
+/* 在php源码树外面构建时引入配置选项 */
 #ifdef HAVE_CONFIG_H
 #include "config.h"
 #endif
-
-//包含PHP的标准头文件
+/* 包含php的标准头文件 */
 #include "php.h"
-
-//定义入口点符号， zend在加载这个模块的时候使用
-extern  zend_module_entry sample_module_entry;
+/* 定义入口点符号, Zend在加载这个模块的时候使用 */
+extern zend_module_entry sample_module_entry;
 #define phpext_sample_ptr &sample_module_entry
+#endif /* PHP_SAMPLE_H */
 
-#endif  
-
-//PHP_SAMPLE_H
