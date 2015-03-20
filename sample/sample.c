1466fee6bb4bfe0cb72cb43fe602b7aa1521dc11
root <root@keeg>
2015-03-21 06:33:03 +0800

    x

---
+++ sample/sample.c
@@ -0,0 +1,27 @@
+#include "php_sample.h"
+
+zend_module_entry sample_module_entry = {
+	#if	ZEND_MODULE_API_NO >= 20010901
+		STANDARD_MODULE_HEADER,
+	#endif
+	
+		PHP_SAMPLE_EXTNAME,
+		NULL,	//Functions
+		NULL,	//MINIT
+		NULL,	//MSHUTDOWN
+		NULL,	//RINIT
+		NULL,	//RSHUTDOWN
+		NULL,	//MINFO
+		
+	#if ZEND_MODULE_API_NO >= 20010901
+		PHP_SAMPLE_EXTNAME,
+	#endif
+		STANDARD_MODULE_PROPERTIES
+};
+
+#ifdef COMPILE_DL_SAMPLE
+ZEND_GET_MODULE(sample)
+#endif
+
+
+
