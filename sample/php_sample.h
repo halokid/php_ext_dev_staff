5cb266342dcef48a8d018da49420c60b2381e52e
r00tyy <82049406@qq.com>
2015-04-28 18:10:50 +0800

    add files

---
+++ sample/php_sample.h
@@ -0,0 +1,14 @@
+#ifdef PHP_SAMPLE_H
+
+#define PHP_SAMPLE_H
+
+#define PHP_SAMPLE_EXTNAME	"sample"
+#define PHP_SAMPLE_EXTVER		"1.0"
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include "php.h"
+
+extern 
